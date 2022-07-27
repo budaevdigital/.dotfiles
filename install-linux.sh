5d0809bc9f46ee88d0fd902081db614b2d46a1c6
Dmitry Budaev <budaev.digital@gmail.com>
2022-07-27 21:31:44 +0500

    сжатие директории

--- install-linux.sh
+++ install-linux.sh
@@ -4,12 +4,12 @@ DOT_DIR_FILE_HOME_ZIP=~/.dotfiles-master.zip
 ISBASHRC=$1
 REMOTHERFILES=$2
 
-LINK_IN_BASHRC_FILE=('#.bashrc\n\n
-source '$DOT_DIR_FILE_HOME'/bash/.shells/.export\n
-source '$DOT_DIR_FILE_HOME'/bash/.shells/.defaults\n
-source '$DOT_DIR_FILE_HOME'/bash/.shells/.functions\n
-source '$DOT_DIR_FILE_HOME'/bash/.shells/.alias\n
-source '$DOT_DIR_FILE_HOME'/bash/.shells/.prompt')
+LINK_IN_BASHRC_FILE=("#.bashrc\n\n
+source $DOT_DIR_FILE_HOME/bash/.shells/.export\n
+source $DOT_DIR_FILE_HOME/bash/.shells/.defaults\n
+source $DOT_DIR_FILE_HOME/bash/.shells/.functions\n
+source $DOT_DIR_FILE_HOME/bash/.shells/.alias\n
+source $DOT_DIR_FILE_HOME/bash/.shells/.prompt")
 
 if [[ ! -d $DOT_DIR_FILE_HOME ]]; then
     wget https://github.com/budaevdigital/.dotfiles/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
