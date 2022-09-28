3a7d836e126c268fc0e0860496e72f909835b97c
budaevdigital <budaev.digital@gmail.com>
2022-09-28 22:23:51 +0500

    Обновление и автоустановщик при первом запуске Fedora

--- first_settings_Fedora.sh
+++ first_settings_Fedora.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 set -e
 
+MY_HOSTNAME=tools
+
 # Предварительно установить - https://flathub.org/repo/flathub.flatpakrepo
 # Для настройки Flat менеджера пакетов
 
-MY_HOSTNAME=tools
-
 # Установка hostname машины
 sudo hostnamectl set-hostname $MY_HOSTNAME
-
 
 echo "==============================================="
 echo "Обновление пакетного менеджера"
 echo "==============================================="
 
-sudo dnf upgrade --refresh -y
+sudo dnf -y upgrade --refresh
 echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf &&
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf &&
 echo 'keepcache=1' | sudo tee -a /etc/dnf/dnf.conf
 
-sudo dnf autoremove && sudo dnf clean all
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y autoremove && sudo dnf clean all
+sudo dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo flatpak remote-add --if-not-exists fedora oci+https://registry.fedoraproject.org
 
@@ -29,37 +28,44 @@ echo "==============================================="
 echo "Установка драйверов для NVIDIA и мультимедиа"
 echo "==============================================="
 
-sudo dnf groupupdate core -y
+sudo dnf -y groupupdate core
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 sudo dnf groupupdate sound-and-video
-sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel 
-sudo dnf install lame\* --exclude=lame-devel
+sudo dnf -y install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel 
+sudo dnf -y install lame\* --exclude=lame-devel
 sudo dnf group upgrade --with-optional Multimedia
-sudo dnf install akmod-nvidia
-sudo dnf install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings
-sudo dnf install xorg-x11-drv-nvidia-cuda
+sudo dnf -y install akmod-nvidia
+sudo dnf -y install gcc kernel-headers kernel-devel akmod-nvidia xorg-x11-drv-nvidia xorg-x11-drv-nvidia-libs xorg-x11-drv-nvidia-power nvidia-settings
+sudo dnf -y install xorg-x11-drv-nvidia-cuda
 
 
 echo "==============================================="
 echo "Установка дополнительных программных пакетов"
 echo "==============================================="
+# Дополнительный пакетный менеджер
+sudo dnf -y install dnfdragora
+# Кастомизация интерфейса Gnome
+sudo dnf -y install gnome-tweak-tool
+sudo dnf -y install gnome-shell-extension
+# Для очистки мусора и ненужных файлов
+sudo dnf -y install bleachbit
+# Дополнительные пакеты для мониторинга системы
+sudo dnf -y install neofetch htop
 
-sudo dnf install dnfdragora
-sudo dnf install gnome-tweak-tool
-sudo dnf install gnome-shell-extension
-sudo dnf install bleachbit
-sudo dnf install neofetch htop
-
-# установка VSCode
+# Установка VSCode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf -y update
 sudo dnf -y install code
 
+# Установка терминала
 sudo dnf -y install terminator
-sudo flatpak install md.obsidian.Obsidian
-sudo dnf install grub-customizer
-sudo flatpak install flathub io.github.hmlendea.geforcenow-electron
+# Пакет для ведения заметок
+sudo flatpak -y install md.obsidian.Obsidian
+# Пакет для настройки Grub при загрузки системы
+sudo dnf -y install grub-customizer
+# Пакет для облачного гейминга
+sudo flatpak -y install flathub io.github.hmlendea.geforcenow-electron
 
 
 echo "==============================================="
--- install-linux.sh
+++ install-linux.sh
@@ -2,7 +2,9 @@
 DOT_DIR_FILE_HOME=~/.dotfiles-master
 DOT_DIR_FILE_HOME_ZIP=~/.dotfiles-master.zip
 ISBASHRC=$1
-REMOTHERFILES=$2
+FIRSTSTARTFEDORA=$2
+CONFIGTERMINAL=$3
+REMOTHERFILES=$4
 
 LINK_IN_BASHRC_FILE=("#.bashrc\n\n
 source $DOT_DIR_FILE_HOME/bash/.shells/.export\n
@@ -12,36 +14,54 @@ source $DOT_DIR_FILE_HOME/bash/.shells/.alias\n
 source $DOT_DIR_FILE_HOME/bash/.shells/.prompt")
 
 if [[ ! -d $DOT_DIR_FILE_HOME ]]; then
-    wget https://github.com/budaevdigital/.dotfiles/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
-    mkdir $DOT_DIR_FILE_HOME
-    unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
-    rm $DOT_DIR_FILE_HOME_ZIP
+  wget https://github.com/budaevdigital/.dotfiles/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
+  mkdir $DOT_DIR_FILE_HOME
+  unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
+  rm $DOT_DIR_FILE_HOME_ZIP
 else
-    rm -r $DOT_DIR_FILE_HOME
-    wget https://github.com/budaevdigital/.dotfiles/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
-    mkdir $DOT_DIR_FILE_HOME
-    unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
-    rm $DOT_DIR_FILE_HOME_ZIP    
+  rm -r $DOT_DIR_FILE_HOME
+  wget https://github.com/budaevdigital/.dotfiles/archive/master.zip -O $DOT_DIR_FILE_HOME_ZIP
+  mkdir $DOT_DIR_FILE_HOME
+  unzip $DOT_DIR_FILE_HOME_ZIP -d ~/
+  rm $DOT_DIR_FILE_HOME_ZIP    
 fi
 
+
+# Удаляет bashrc и создаёт ссылку на файл в директории DOT_DIR_FILE_HOME
 if [[ $ISBASHRC == "y" || $ISBASHRC == "Y1" || $ISBASHRC == "" ]]; then
-    if [[ -f ~/.bashrc ]]; then
-	    rm ~/.bashrc
-    fi
-        echo -e $LINK_IN_BASHRC_FILE > $DOT_DIR_FILE_HOME/bash/.bashrc
-        ln -s $DOT_DIR_FILE_HOME/bash/.bashrc ~/.bashrc
-        source ~/.bashrc
+  if [[ -f ~/.bashrc ]]; then
+    rm ~/.bashrc
+  fi
+  echo -e $LINK_IN_BASHRC_FILE > $DOT_DIR_FILE_HOME/bash/.bashrc
+  ln -s $DOT_DIR_FILE_HOME/bash/.bashrc ~/.bashrc
+  source ~/.bashrc
 else
-    echo 'Bashrс - пропущен'
+  echo 'Bashrс - пропущен'
 fi
 
-if [[ $REMOTHERFILES == "y" || $REMOTHERFILES == "Y2" || $REMOTHERFILES == "" ]]; then    
-    rm -r $DOT_DIR_FILE_HOME/_assets
-    rm -r $DOT_DIR_FILE_HOME/terminator
-    rm $DOT_DIR_FILE_HOME/install-linux.sh
-    rm $DOT_DIR_FILE_HOME/README.md
+if [[ $FIRSTSTARTFEDORA == "y" || $FIRSTSTARTFEDORA == "Y2" || $FIRSTSTARTFEDORA == "" ]]; then
+  bash $DOT_DIR_FILE_HOME/first_settings_Fedora.sh
 else
-    echo 'Установочные файлы были нетронуты'
+    echo 'Первоначальная настройка Fedora Linux пропущена'
+fi
+
+if [[ $CONFIGTERMINAL == "y" || $CONFIGTERMINAL == "Y3" || $CONFIGTERMINAL == "" ]]; then
+  if [[ -f ~/.config/terminator/config ]]; then
+    cp -f cp $DOT_DIR_FILE_HOME/terminator/config ~/.config/terminator/config
+  fi
+  cp $DOT_DIR_FILE_HOME/terminator ~/.config/terminator/
+else
+    echo 'Конфиг для terminator пропущен'
+fi
+
+if [[ $REMOTHERFILES == "y" || $REMOTHERFILES == "Y4" || $REMOTHERFILES == "" ]]; then    
+  rm -r $DOT_DIR_FILE_HOME/_assets
+  rm -r $DOT_DIR_FILE_HOME/terminator
+  rm $DOT_DIR_FILE_HOME/install-linux.sh
+  rm $DOT_DIR_FILE_HOME/first_settings_Fedora.sh
+  rm $DOT_DIR_FILE_HOME/README.md
+else
+  echo 'Установочные файлы были нетронуты'
 fi
 
 echo 'Поздравляем! Всё установлено и готово к использованию!'
